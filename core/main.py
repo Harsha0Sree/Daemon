232801fba5a37ba0efa4969af2213779acf19b8b
Harsha0Sree <mikeysama@Mikey.localdomain>
2026-05-19 19:38:28 -0700

    transition to ORM completely and add jinja functionality

--- core/main.py
+++ core/main.py
@@ -1,14 +1,12 @@
-import sqlite3
 from datetime import date
-from database import SessionLocal,Base,engine
-from fastapi import FastAPI, Request
+from database import Base, SessionLocal, engine
+from fastapi import FastAPI, Form, Request
+from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from models import Habit
+from models import Habit, Logs
 from pydantic import BaseModel
 from sqlalchemy import select
-
-DATABASE = "data.db"
 
 Base.metadata.create_all(engine)
 
@@ -22,54 +20,6 @@ class HabitToUpdate(BaseModel):
     name_to_update_to: str
 
 
-def create_habit_table(DATABASE):
-    with DatabaseConnection(DATABASE) as conn:
-        cursor = conn.cursor()
-        cursor.execute("""CREATE TABLE IF NOT EXISTS habits   (
-                        id INTEGER PRIMARY KEY,
-                        name TEXT UNIQUE NOT NULL,
-                        created_at TEXT,
-                        description TEXT
-                                
-                )
-                """)
-
-
-def create_logs_table(DATABASE):
-    with DatabaseConnection(DATABASE) as conn:
-        cursor = conn.cursor()
-        cursor.execute("PRAGMA foreign_keys = ON")
-        cursor.execute("""CREATE TABLE IF NOT EXISTS logs(
-                       id INTEGER PRIMARY KEY,
-                       log_date TEXT NOT NULL,
-                       habit_id INTEGER NOT NULL,
-                       FOREIGN KEY (habit_id) REFERENCES habits(id)
-                       ON DELETE CASCADE
-                       )
-                        """)
-
-
-class DatabaseConnection:
-    def __init__(self, database_path):
-        self.database_path = database_path
-        self.conn = None
-
-    def __enter__(self):
-        self.conn = sqlite3.connect(self.database_path)
-        self.conn.row_factory = sqlite3.Row
-        return self.conn
-
-    def __exit__(self, exc_type, exc, tb):
-        if self.conn:
-            if exc_type is None:
-                self.conn.commit()
-            else:
-                self.conn.rollback()
-            self.conn.close()
-
-
-create_habit_table(DATABASE)
-create_logs_table(DATABASE)
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -92,71 +42,59 @@ def get_dashboard(request: Request):
 
 @app.post("/habits")
 def create_habit(habit: HabitCreate):
-    with DatabaseConnection(DATABASE) as conn:
-        cursor = conn.cursor()
-        cursor.execute("INSERT INTO habits (name) VALUES (?)", (habit.name,))
-        habit_id = cursor.lastrowid
+    with SessionLocal() as session:
+        new_row = Habit(habit_name=habit.name)
+        session.add(new_row)
+        session.commit()
 
     return {
         "message": f"a new habit {habit} is created",
-        "habit_id": habit_id,
     }
 
 
 @app.get("/habits")
 def get_all_habits():
-    with DatabaseConnection(DATABASE) as conn:
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM habits")
-        habits = cursor.fetchall()
-    dict_habit = []
-    for habit in habits:
-        dict_habit.append({"id": habit["id"], "name": habit["name"]})
-    return {"data": dict_habit}
+    with SessionLocal() as session:
+        row = select(Habit)
+        habits = session.execute(row).scalars().all()
+
+    return {"data": habits}
 
 
 @app.get("/habits/{name}")
 def get_one_habit(name: str):
-    conn = sqlite3.connect(DATABASE)
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM habits WHERE name = (?)", (name,))
-    habit = cursor.fetchone()
+    with SessionLocal() as session:
+        row = select(Habit).where(Habit.habit_name == name)
+        habit = session.execute(row).scalar_one_or_none()
 
-    conn.close()
-    if habit is None:
-        return "habit not found"
-    return {"id": habit["id"], "name": habit["name"]}
+        if habit is None:
+            return "habit not found"
+        return {"id": habit.id, "name": habit.habit_name}
 
 
 @app.delete("/habits/{name}")
 def delete_habit(name: str):
-    conn = sqlite3.connect(DATABASE)
-    cursor = conn.cursor()
-    cursor.execute("DELETE FROM habits WHERE name = ?", (name,))
-    conn.commit()
-    conn.close()
-    return {"message": f"the habit {name} is deleted"}
+    with SessionLocal() as session:
+        row = select(Habit).where(Habit.habit_name == name)
+        habit_to_delete = session.execute(row).scalar_one_or_none()
+        if habit_to_delete is not None:
+            session.delete(habit_to_delete)
+            session.commit()
+            return {"message": f"the habit {name} is deleted"}
+        return {"message": f"habit {name} not found"}
 
 
 @app.post("/logs/{habit_name_to_log}")
 def log_habit(habit_name_to_log: str):
-    with DatabaseConnection(DATABASE) as conn:
-        cursor = conn.cursor()
-        cursor.execute(
-            """SELECT id FROM habits WHERE name = (?)""", (habit_name_to_log,)
-        )
-        row = cursor.fetchone()
-        if id is not None:
+    with SessionLocal() as session:
+        row = select(Habit).where(Habit.habit_name == habit_name_to_log)
+        habit = session.execute(row).scalar_one_or_none()
+
+        if habit is not None:
             today_date = str(date.today())
-            cursor.execute(
-                """INSERT INTO logs (habit_id, log_date)
-                        VALUES (?,?)
-                        """,
-                (
-                    row["id"],
-                    today_date,
-                ),
-            )
+            logs = Logs(habit_id=habit.id, logs=today_date)
+            session.add(logs)
+            session.commit()
             return {
                 "message": f"the habit {habit_name_to_log} has been logged on {today_date}"
             }
@@ -165,28 +103,35 @@ def log_habit(habit_name_to_log: str):
 
 @app.get("/logs/{habit}")
 def get_logs(habit: str):
-    with DatabaseConnection(DATABASE) as conn:
-        cursor = conn.cursor()
-        cursor.execute("SELECT id FROM habits WHERE name = (?)", (habit,))
-        row = cursor.fetchone()
-        cursor.execute("SELECT * FROM logs WHERE habit_id = (?)", (row["id"],))
-        logs = cursor.fetchall()
+    with SessionLocal() as session:
+        row = select(Habit).where(Habit.habit_name == habit)
+        habit = session.execute(row).scalar_one_or_none()
+        log_row = select(Logs).where(Logs.habit_id == habit.id)
+        logs = session.execute(log_row).scalars().all()
     return {"data": logs}
 
 
+@app.post("/habits/create")
+def create_habit_new(habit_name: str = Form(...)):
+    with SessionLocal() as session:
+        new_habit = Habit(habit_name=habit_name)
+        session.add(new_habit)
+        session.commit()
+    return RedirectResponse(url="/dashboard", status_code=303)
+
+@app.get("/habit/create")
+def test():
+    return "test"
+
 @app.put("/habits/{name}")
-def update_habit(update_name: HabitToUpdate):
-    conn = sqlite3.connect(DATABASE)
-    cursor = conn.cursor()
-    cursor.execute(
-        "UPDATE habits SET name = (?) WHERE name = (?)",
-        (
-            update_name.name_to_update_to,
-            update_name.name,
-        ),
-    )
-    conn.commit()
-    conn.close()
-    return {
-        "message": f"the habit {update_name.name} has been changed to {update_name.name_to_update_to} "
-    }
+def update_habit(name_to_update_to: HabitToUpdate):
+    with SessionLocal() as session:
+        row = select(Habit).where(Habit.habit_name == name_to_update_to.name)
+        habit = session.execute(row).scalar_one_or_none()
+        if habit is not None:
+            habit.habit_name = name_to_update_to.name_to_update_to
+            session.commit()
+
+        return {
+            "message": f"the habit {name_to_update_to.name} has been changed to {name_to_update_to.name_to_update_to} "
+        }
