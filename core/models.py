90fe493ba7328f7e9f5e68a5d3339f6d47f63aa6
Harsha0Sree <harshaskavish@gmail.com>
2026-05-19 19:38:28 -0700

    transition to ORM completely and add jinja functionality

--- core/models.py
+++ core/models.py
@@ -1,8 +1,16 @@
 from database import Base
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
 
 class Habit(Base):
     __tablename__ = "habits"
     habit_name: Mapped[str] = mapped_column(unique=True)
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+
+class Logs(Base):
+    __tablename__ = "logs"
+    habit_id: Mapped[int] = mapped_column(ForeignKey("habits.id"))
+    logs: Mapped[str]
     id: Mapped[int] = mapped_column(primary_key=True)
